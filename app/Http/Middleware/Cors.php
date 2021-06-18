e87e423b6ce5d20a9d22931d2ee55fcf78964970
pogrebnyak.r <coder.ept@gmail.com>
2021-06-18 11:56:37 +0600

    Доработка

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,26 +1,28 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
 use http\Client\Response;
 
-class Cors {
+class Cors
+{
 
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
 
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', 'http://192.168.5.129')
             ->header('Access-Control-Allow-Methods', '*')
             ->header('Access-Control-Allow-Credentials', true)
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization,X-Auth-Token')
             ->header('Accept', 'application/json');
     }
 
