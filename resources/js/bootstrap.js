5f3984fb3968423bda19111dd8d49017f2a472cd
pogrebnyak.r <coder.ept@gmail.com>
2021-06-18 12:18:30 +0600

    Доработка

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,7 +21,7 @@ try {
 
 window.axios = require('axios');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 let token = document.querySelector('meta[name="csrf-token"]');
 
 if (token) {
